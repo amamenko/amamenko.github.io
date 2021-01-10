df308a8e6d4f680abf99ddc68cc2ca84122d9c01
amamenko <amamenko@hotmail.com>
2021-01-10 00:36:36 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-abbe6ba6500c458c3627.js"
+    "url": "webpack-runtime-9ef6c876b03e51357189.js"
   },
   {
     "url": "framework-6c3ead24af170cdcb87c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8e50cb64999a45811913932368388bed"
+    "revision": "9978cab1b27df1651121c49a05e154ba"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-260e89c911c4cfae04b1.js"
